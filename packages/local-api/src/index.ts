534f104cf3abd94135481d5850ba476bb413dc2c
Qandeel Khan <com.qandeelhaider@gmail.com>
2022-04-24 20:57:40 +0500

    initial commit

--- packages/local-api/src/index.ts
+++ packages/local-api/src/index.ts
@@ -1,34 +1,34 @@
-import express from "express";
-import { createProxyMiddleware } from "http-proxy-middleware";
-import path from "path";
-import { createCellsRouter } from "./routes/cells";
+import express from 'express';
+import { createProxyMiddleware } from 'http-proxy-middleware';
+import path from 'path';
+import { createCellsRouter } from './routes/cells';
 
 export const serve = (
-    port: number,
-    filename: string,
-    dir: string,
-    useProxy: boolean
+  port: number,
+  filename: string,
+  dir: string,
+  useProxy: boolean
 ) => {
-    const app = express();
+  const app = express();
 
-    if (useProxy) {
-        app.use(
-            createProxyMiddleware({
-                target: "http://localhost:3000",
-                ws: true,
-                logLevel: "silent",
-            })
-        );
-    } else {
-        const packagePath = require.resolve(
-            "@jsbook-qandeel/local-client/build/index.html"
-        );
-        app.use(express.static(path.dirname(packagePath)));
-    }
+  app.use(createCellsRouter(filename, dir));
 
-    app.use(createCellsRouter(filename, dir));
+  if (useProxy) {
+    app.use(
+      createProxyMiddleware({
+        target: 'http://localhost:3000',
+        ws: true,
+        logLevel: 'silent',
+      })
+    );
+  } else {
+    const packagePath = require.resolve(
+      '@jsnote/local-client/build/index.html'
+    );
+    app.use(express.static(path.dirname(packagePath)));
+  }
 
-    return new Promise<void>((resolve, reject) => {
-        app.listen(port, resolve).on("error", reject);
-    });
+  return new Promise<void>((resolve, reject) => {
+    app.listen(port, resolve).on('error', reject);
+  });
 };
